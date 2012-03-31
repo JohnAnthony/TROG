1835c5cf8388a627d34d2631692331e702584ace
John Anthony <johnanthony@lavabit.com>
2012-03-31 06:48:54 +0100

    Fixed a going-up-stairs bug

--- game.cpp
+++ game.cpp
@@ -137,7 +137,7 @@ Game::GoDownALevel(void) {
 
     this->cur_level = this->cur_level->next;
 
-    this->character->MoveTo(this->cur_level->stairs_up);
+    this->character->MoveTo(this->cur_level->stairs_down);
     this->cur_level->RevealSight(this->character);
     this->cur_level->CentreCam(this->character->pos);
     this->DoRedraw();
