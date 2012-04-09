57a05f902a43ee72195f7fe54b5961d52e47d0be
John Anthony <johnanthony@lavabit.com>
2012-04-09 15:45:55 +0100

    Changed low-level enemy descriptions to be less grandiose

--- enemy.hpp
+++ enemy.hpp
@@ -27,9 +27,9 @@ class Enemy : public Entity {
 
 static Enemy EnemyList[] = {
 
-//lvl       sym     col         sig     hp  mp  str tou att def mag wil mv      xp      name
-Enemy(1,    'o',    COL_BLUE,   5,      1,  0,  5,  5,  10, 10, 0,  5,  950,    50,     "Binary Blob", "insidious"),
-Enemy(1,    'B',    COL_YELLOW, 5,      10, 0,  10, 10, 5,  5,  0,  5,  1000,   125,    "Neckbear", "ferocious"),
+//lvl       sym     col         sight   hp  mp  str tou att def mag wil mv      xp      name
+Enemy(1,    'o',    COL_BLUE,   5,      1,  0,  5,  5,  10, 10, 0,  5,  950,    50,     "Binary Blob", "petty"),
+Enemy(1,    'b',    COL_YELLOW, 5,      10, 0,  10, 10, 5,  5,  0,  5,  1000,   125,    "Neckbear", "juvenile"),
 
 
 };
