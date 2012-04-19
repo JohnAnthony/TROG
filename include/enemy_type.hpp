4b6972c02e55b6a69171211d34799c81acc9ba2c
John Anthony <johnanthony@lavabit.com>
2012-04-19 12:27:22 +0100

    Rebalancing

--- include/enemy_type.hpp
+++ include/enemy_type.hpp
@@ -32,16 +32,16 @@ static EnemyType EnemyTypeList[] = {
 EnemyType(1,    'z',    COL_GREEN,  6,      5,  0,  10, 5,  10, 5,  0,  5,  1500,   55,     "Decrepit Zombie"),
 EnemyType(1,    'r',    COL_YELLOW, 6,      5,  0,  10, 5,  10, 5,  0,  5,  1100,   65,     "Giant Rat"),
 EnemyType(1,    'a',    COL_RED,    6,      5,  0,  10, 5,  10, 5,  0,  5,  1100,   65,     "Giant Fire Ant"),
-EnemyType(2,    'b',    COL_BLUE,   20,     10, 0,  15, 7,  15, 7,  0,  7,  1100,   185,    "Giant Cave Bat"),
-EnemyType(2,    's',    COL_WHITE,  6,      10, 0,  15, 7,  15, 7,  0,  7,  800,    225,    "Decrepit Skeleton"),
-EnemyType(3,    'c',    COL_GREEN,  6,      15, 0,  20, 10, 20, 10, 0,  10, 1200,   350,    "Cave Crocodile"),
-EnemyType(4,    'S',    COL_GREEN,  6,      20, 0,  25, 12, 25, 12, 0,  12, 1100,   450,    "Tomb Serpent"),
-EnemyType(5,    's',    COL_PURPLE, 6,      25, 0,  30, 15, 30, 15, 0,  15, 1000,   700,    "Shade"),
-EnemyType(6,    'O',    COL_YELLOW, 6,      30, 0,  35, 17, 35, 17, 0,  17, 1200,   850,    "Owlbear"),
-EnemyType(7,    'g',    COL_PURPLE, 6,      35, 0,  40, 20, 40, 20, 0,  20, 1000,   900,    "Doppelganger"),
-EnemyType(8,    'n',    COL_RED,    6,      40, 0,  45, 22, 45, 22, 0,  22, 1000,   1050,   "Fire Newt"),
-EnemyType(9,    'R',    COL_WHITE,  6,      45, 0,  50, 25, 50, 25, 0,  25, 1200,   1200,   "Grave Robber"),
-EnemyType(10,   'T',    COL_GREEN,  6,      50, 0,  55, 27, 55, 27, 0,  27, 1000,   1000,   "Cave Troll")
+EnemyType(2,    'b',    COL_BLUE,   20,     15, 0,  15, 7,  15, 7,  0,  7,  1100,   185,    "Giant Cave Bat"),
+EnemyType(2,    's',    COL_WHITE,  6,      15, 0,  15, 7,  15, 7,  0,  7,  800,    225,    "Decrepit Skeleton"),
+EnemyType(3,    'c',    COL_GREEN,  6,      25, 0,  20, 10, 20, 10, 0,  10, 1200,   350,    "Cave Crocodile"),
+EnemyType(4,    'S',    COL_GREEN,  6,      40, 0,  25, 12, 25, 12, 0,  12, 1100,   450,    "Tomb Serpent"),
+EnemyType(5,    's',    COL_PURPLE, 6,      60, 0,  30, 15, 30, 15, 0,  15, 1000,   700,    "Shade"),
+EnemyType(6,    'O',    COL_YELLOW, 6,      85, 0,  35, 17, 35, 17, 0,  17, 1200,   850,    "Owlbear"),
+EnemyType(7,    'g',    COL_PURPLE, 6,      105,0,  40, 20, 40, 20, 0,  20, 1000,   900,    "Doppelganger"),
+EnemyType(8,    'n',    COL_RED,    6,      130,0,  45, 22, 45, 22, 0,  22, 1000,   1050,   "Fire Newt"),
+EnemyType(9,    'R',    COL_WHITE,  6,      160,0,  50, 25, 50, 25, 0,  25, 1200,   1200,   "Grave Robber"),
+EnemyType(10,   'T',    COL_GREEN,  6,      200,0,  55, 27, 55, 27, 0,  27, 1000,   1000,   "Cave Troll")
 
 };
 
